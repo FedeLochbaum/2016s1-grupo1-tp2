9b8f80671a73fee87bfc5d99cdd0808ad4f489e0
trimegisto <federico.lochbaum@gmail.com>
2016-04-15 09:45:29 -0300

    Implementacion de affected_classes, affected_methods y affects_method? en Cut

--- model/point_cut.rb
+++ model/point_cut.rb
@@ -5,11 +5,31 @@ class Point_cut
   end
 
   def affected_classes
+    obtenerTodasLasClases.filter {|a_class| algunoCumple a_class}
+  end
 
+  def algunoCumple(a_class)
+    a_class.instance_methods.any? {|selector| affects_method?(a_class,selector)}
   end
 
   def affected_methods
+      res = []
+      obtenerTodasLasClases.each {|a_class| agregarSelectoresQueCumplen(a_class,res)}
+      res
+  end
 
+  def agregarSelectoresQueCumplen(a_class,lista)
+    a_class.instance_methods.each {|selector| agregaSiCumple(selector,a_class,lista)}
+  end
+
+  def agregaSiCumple(selector,a_class,lista)
+    if(affects_method? a_class,selector)
+      lista.add(a_class,selector)
+    end
+  end
+
+  def obtenerTodasLasClases
+    ObjectSpace.each_object(Class)
   end
 
   def affects_method?(a_class,selector)
